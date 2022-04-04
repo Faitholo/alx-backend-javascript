1e467e5e6b4290272a29026738f9eff09fa127af
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-04-04 23:28:18 +0100

    task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-param-reassign */
+export default function appendToEachArrayValue(array, appendString) {
+  for (const idx of array) {
+    const value = array[idx];
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}
