6948dc6ae3cb8f8fe1657a00a0eb6b3337473634
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-04-04 13:46:00 +0100

    fixing again

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    // eslint-disable-next-line no-unused-vars
+    const task = true;
+    // eslint-disable-next-line no-unused-vars
+    const task2 = false;
   }
 
   return [task, task2];
