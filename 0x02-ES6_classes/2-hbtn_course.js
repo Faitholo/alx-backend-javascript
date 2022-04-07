e8529faea194f6ff052a3835e1ece77eb9a21fb9
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-04-07 22:59:32 +0100

    brain bursting task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-underscore-dangle */
+class HolbertonCourse {
+  constructor(name = '', length = 0, students = []) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(name = '') {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(length = 0) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(students = []) {
+    if (typeof students !== 'object') {
+      throw TypeError('Students must be an array');
+    }
+    this._students = students;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
+export default HolbertonCourse;
