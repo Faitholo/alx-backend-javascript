6aca4480117d6d3400b5442f5140b1043ed0161e
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-04-07 23:24:50 +0100

    brain bursting task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -29,7 +29,7 @@ class HolbertonCourse {
   }
 
   set students(students = []) {
-    if (students instanceof Array) {
+    if (typeof students !== 'object') {
       throw TypeError('Students must be an array');
     }
     this._students = students;
