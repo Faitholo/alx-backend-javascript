70aafec7718305e9657676cf359663ea3b5bf42a
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-04-08 10:42:40 +0100

    attempting task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unreachable */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-unused-vars */
+import Building from './5-building';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly ${this._floors} floors`;
+  }
+}
