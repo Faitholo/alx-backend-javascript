a220d82c084989fa8ebf661ce04e4926c5031fce
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-04-08 12:31:37 +0100

    fixing one error

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,21 @@
+/* eslint-disable no-unreachable */
 /* eslint-disable no-underscore-dangle */
 class HolbertonCourse {
   constructor(name = '', length = 0, students = []) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+      this._name = name;
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+      this._length = length;
+    }
+
+    if (typeof students !== 'object') {
+      throw TypeError('Students must be an array');
+      this._students = students;
+    }
   }
 
   set name(name = '') {
