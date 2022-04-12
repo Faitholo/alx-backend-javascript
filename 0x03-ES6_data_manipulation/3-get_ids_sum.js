cc0bb71149aca7d9d77d80afaeb86aa4a00f090e
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-04-12 08:39:39 +0100

    task 3

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+export default function getStudentIdsSum(getListStudents) {
+  const myArr = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return myArr.reduce(((reducer, myArr) => reducer + myArr.id), 0);
+}
